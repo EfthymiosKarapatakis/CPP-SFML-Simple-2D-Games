79131c28c0e4c8dbf4279ac3949e1820bb47abf0
efthimis.krpt <karapatakisef@gmail.com>
2025-08-26 12:15:15 +0300

    Create SwagBall.cpp
    Create enemy class

---
+++ Game2/src/SwagBall.cpp
@@ -0,0 +1,22 @@
+#include "SwagBall.h"
+
+void Swagball::initShape() {
+
+}
+
+Swagball::Swagball() {
+
+}
+
+Swagball::~Swagball() {
+    
+}
+
+// Functions
+void Swagball::update() {
+
+}
+
+void Swagball::render(sf::RenderTarget & target) {
+
+}
