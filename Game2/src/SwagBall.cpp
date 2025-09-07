17e2a59a5d80fd0fa7c282f051adfbefcea0dd35
efthimis.krpt <karapatakisef@gmail.com>
2025-09-07 12:33:48 +0300

    Update SwagBall.cpp
    - Swag ball types

--- Game2/src/SwagBall.cpp
+++ Game2/src/SwagBall.cpp
@@ -2,17 +2,36 @@
 
 void Swagball::initShape(const sf::RenderWindow& window) {
     this->shape.setRadius(static_cast<float>(rand() % 10 + 10));    // random size
-    sf::Color color(rand() % 255 + 1, rand() % 255 + 1, rand() %255 + 1);   // random color
+
+    // Set color
+    sf::Color color;
+    switch (this->type)
+    {
+    case DEFAULT:
+        color = sf::Color::Blue;
+        break;
+    case DAMAGING:
+        color = sf::Color::Red;
+        break;
+    case HEALING:
+        color = sf::Color::Green;
+        break;
+    default:
+        break;
+    }
+
+
+    
     this->shape.setFillColor(color);
     this->shape.setPosition(
         sf::Vector2f(
             static_cast<float>(rand() % window.getSize().x - this->shape.getGlobalBounds().width),
             static_cast<float>(rand() % window.getSize().y - this->shape.getGlobalBounds().height)
         )
-    );
+    ); 
 }
 
-Swagball::Swagball(const sf::RenderWindow& window) {
+Swagball::Swagball(const sf::RenderWindow& window, int type) : type(type) {
     this->initShape(window);
 }
 
@@ -25,6 +44,9 @@ const sf::CircleShape Swagball::getShape() const {
     return this->shape;
 }
 
+const int Swagball::getType() const {
+    return this->type;
+}
 
 void Swagball::update() {
 
