2ac2777606bba955139591717cd2d239c027e1c1
efthimis.krpt <karapatakisef@gmail.com>
2025-09-10 14:04:26 +0300

    Update main.cpp

--- Game2/src/main.cpp
+++ Game2/src/main.cpp
@@ -8,7 +8,7 @@ int main() {
     Game game;
 
     // Game loop
-    while (game.running()) {
+    while (game.running() /*&& !game.getEndGame()*/) { // with this when hp<=0 the game will close with no end game text
         game.update();
         game.render();
     }
